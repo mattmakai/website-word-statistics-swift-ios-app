f144bf87fe41c40a9138ed07b572c7cf238ae8e3
Matt Makai <matthew.makai@gmail.com>
2016-09-01 18:00:00 -0700

    Initial Commit

---
+++ Website Word Statistics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Website Word Statistics
+//
+//  Created by Matthew Makai on 9/1/16.
+//  Copyright © 2016 Matt Makai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
